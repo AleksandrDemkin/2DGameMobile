dd30778d379bb16e3a890bc50cef27cff8462868
AleksandrDemkin <djomadjomich@gmail.com>
2022-02-03 02:00:18 +0200

    Task9

---
+++ Assets/Scripts/DataSpriteBundle.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine.UI;
+
+namespace Bundles
+{
+    [Serializable]
+    public class DataSpriteBundle
+    {
+        public string NameAssetBundle;
+        public Image Image;
+    }
+}
